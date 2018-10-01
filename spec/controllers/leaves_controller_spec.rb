0cb26f435fd9f574d9e707ed7fe4a2a9afa56be3
wichru <wicher.maciej@gmail.com>
2018-10-01 09:35:27 +0200

    model tests added using shoulda matchers

--- spec/controllers/leaves_controller_spec.rb
+++ spec/controllers/leaves_controller_spec.rb
@@ -13,31 +13,31 @@ RSpec.describe LeavesController, type: :controller do
   describe "GET 'new'" do
     it 'returns http success' do
       get :new
-      response.should be_successful
+      response.is_expected be_successful
     end
   end
   describe "GET 'create'" do
     it 'returns http success' do
       get :create
-      response.should be_successful
+      is_expected be_successful
     end
   end
   describe "GET 'edit'" do
     it 'returns http success' do
       get :edit
-      response.should be_successful
+      is_expected be_successful
     end
   end
   describe "GET 'update'" do
     it 'returns http success' do
       get :update
-      response.should be_successful
+      is_expected be_successful
     end
   end
   describe "GET 'destroy'" do
     it 'returns http success' do
       get :destroy
-      response.should be_successful
+      is_expected be_successful
     end
   end
 end
--- spec/models/leave_spec.rb
+++ spec/models/leave_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Leave, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:start_date) }
 end
