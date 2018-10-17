04ee55d4e6ce6a59d873226cf2c3bfa94bb5d129
wichru <wicher.maciej@gmail.com>
2018-10-17 23:57:20 +0200

    changed root for all admin views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
+  def after_sign_in_path_for(resource)
+    if resource.admin == true
+      admin_path
+    else
+      leaves_path
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
