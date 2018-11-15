3c16ab48d50804556c5b49cf81170cf95fae386f
wichru <wicher.maciej@gmail.com>
2018-11-16 00:15:52 +0100

    Add shoulda matchers gem for RSpec validations

--- spec/models/leave_spec.rb
+++ spec/models/leave_spec.rb
@@ -1,35 +1,13 @@
-require 'rails_helper'
-
 RSpec.describe Leave, type: :model do
 
   describe "validations" do
-    let(:user) { User.create(email: 'user@example.com', password: '0ksymoroN') }
-    let(:leave) { Leave.new(start_date: start_date, end_date: end_date) }
-
-    let(:start_date) { 2018 - 10 - 21 }
-    let(:end_date) { 2018 - 10 - 22 }
-
-    subject { leave.valid? }
+    it { is_expected.to validate_presence_of(:start_date) }
+    it { is_expected.to validate_presence_of(:end_date) }
 
     context "when start date isn't before end date" do
+      subject { Leave.new(start_date: start_date, end_date: end_date).valid? }
       let(:start_date) { 2018-10-23}
       let(:end_date) { 2018-10-22}
-
-      it "is invalid" do
-        is_expected.to eq false
-      end
-    end
-
-    context "when start date doesn't exist" do
-      let(:start_date) { nil }
-
-      it "is invalid" do
-        is_expected.to eq false
-      end
-    end
-
-    context "when end date doesn't exist" do
-      let(:end_date) { nil }
 
       it "is invalid" do
         is_expected.to eq false
