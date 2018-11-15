86be04544b741421c2b5ce959af0241c718f1629
wichru <wicher.maciej@gmail.com>
2018-11-15 21:31:10 +0100

    Leave model validation tests without subject

--- spec/models/leave_spec.rb
+++ spec/models/leave_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Leave, type: :model do
-  it { is_expected.to validate_presence_of(:start_date) }
+
+  describe "validations" do
+    let(:user) { User.create(email: 'user@example.com', password: '0ksymoroN') }
+    let(:leave) { Leave.new(start_date: start_date, end_date: end_date) }
+
+    let(:start_date) { 2018 - 10 - 21 }
+    let(:end_date) { 2018 - 10 - 22 }
+
+    context "when start date isn't before end date" do
+      let(:start_date) { 2018-10-23}
+      let(:end_date) { 2018-10-22}
+
+      it "is invalid" do
+        expect(leave.valid?).to eq false
+      end
+    end
+
+    context "when start date doesn't exist" do
+      let(:start_date) { nil }
+
+      it "is invalid" do
+        expect(leave.valid?).to eq false
+      end
+    end
+
+
+    context "when end date doesn't exist" do
+      let(:end_date) { nil }
+
+      it "is invalid" do
+        expect(leave.valid?).to eq false
+      end
+    end
+  end
+
 end
