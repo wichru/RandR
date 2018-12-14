25ff3cec8b93fac542fbdbb2ccbcfdcf7fc12d53
wichru <wicher.maciej@gmail.com>
2018-12-14 23:17:18 +0100

    Add missing home specs

--- spec/controllers/home_controller_test.rb
+++ spec/controllers/home_controller_test.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
-  describe 'GET #index' do
-    it 'returns HTTP success' do
-      get :index
-      expect(response).to be_successful
+  describe 'signed user' do
+    before do
+      user = User.create!(
+        email: 'example@example.com',
+        password: 'test123',
+        first_name: 'John',
+        last_name: 'Kowalski',
+        admin: false
+      )
+      sign_in
+    end
+
+    describe 'GET #index' do
+      it 'returns HTTP success' do
+        get :index
+        expect(response).to redirect_to(leaves_path)
+      end
     end
   end
 end
