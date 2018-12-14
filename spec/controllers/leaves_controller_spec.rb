ab5bc2ec00936b735a99ef420db88a9d97bdc2ee
wichru <wicher.maciej@gmail.com>
2018-12-14 23:36:57 +0100

    Create leaves for non-asigned users in spec

--- spec/controllers/leaves_controller_spec.rb
+++ spec/controllers/leaves_controller_spec.rb
@@ -10,6 +10,38 @@ RSpec.describe LeavesController, type: :controller do
     end
   end
 
+  describe 'POST #create' do
+    context 'when leaves are creating' do
+      let(:valid_params) do
+        { leave:
+          { start_date: 2018-11-15,
+            end_date: 2018-11-19,
+            leave_type: 'Holiday' }}
+      end
+
+      let(:invalid_params) do
+        { leave:
+          { start_date: 2018-11-20,
+            end_date: 2018-11-19,
+            leave_type: 'Holiday' }}
+      end
+
+      it 'it creates leave request' do
+        expect do
+          post :create, params: valid_params
+        end.to change(Leave, :count).by(1)
+        expect(response).to redirect_to(leaves_path)
+      end
+
+      it "doesn't create leave request" do
+        expect do
+          post :create, params: invalid_params
+        end.to change(Leave, :count)
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
   describe "GET 'new'" do
     it 'returns http success' do
       get :new
